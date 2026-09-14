3ec90581b72048cdff9c6dffadcb74f454ec2ba9
Manel <emanuelbatista642s@gmail.com>
2026-09-14 11:04:23 -0400

    Update UsuarioDAO.cs

--- DeltaStock/dao/UsuarioDAO.cs
+++ DeltaStock/dao/UsuarioDAO.cs
@@ -5,13 +5,15 @@ namespace DeltaStock.DAO
     public class Usuario
     {
 
+
+
         //id_usu int primary key auto_increment,
-  // nome_usu varchar(300),
-    //email_usu varchar(300),
-    //senha_usu varchar(300),
-    //telefone_usu varchar(100),
-   // endereco_usu varchar(300),
-    //tipo_usu varchar(100),
-    //status_usu varchar(100)
+        // nome_usu varchar(300),
+        //email_usu varchar(300),
+        //senha_usu varchar(300),
+        //telefone_usu varchar(100),
+        // endereco_usu varchar(300),
+        //tipo_usu varchar(100),
+        //status_usu varchar(100)
     }
 }
