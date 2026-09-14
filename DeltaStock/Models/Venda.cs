de15e5d9f5f97b0c9dc2c98bc6e1c91b075dc182
vinicooo <vinicius8k10@gmail.com>
2026-09-14 10:57:55 -0400

    Update Venda.cs

--- DeltaStock/Models/Venda.cs
+++ DeltaStock/Models/Venda.cs
@@ -1,6 +1,18 @@
 ﻿namespace DeltaStock.Models
 {
-    public class VendaDAO
+    public class Venda
     {
+        public int id { get; set; }
+        public DateOnly dataven { get; set; }
+        public float valortotalven { get; set; }
+        public string statusven { get; set; } = string.Empty;
+        public int idusufk { get; set; }
+
+
+        //id_ven int primary key auto_increment,
+        //data_ven date,
+        //valor_total_ven float,
+        //status_ven varchar(100),
+        //id_usu_fk int
     }
 }
