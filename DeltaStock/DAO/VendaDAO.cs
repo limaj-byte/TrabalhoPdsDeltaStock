ed65ac194a8597597a6472d68b74e90359e33470
vinicooo <vinicius8k10@gmail.com>
2026-09-14 11:19:05 -0400

    Update VendaDAO.cs

--- DeltaStock/DAO/VendaDAO.cs
+++ DeltaStock/DAO/VendaDAO.cs
@@ -1,18 +1,56 @@
-﻿namespace DeltaStock.DAO
+﻿using DeltaStock.Components.Pages.Venda;
+using DeltaStock.Configs;
+
+namespace DeltaStock.DAO
 {
     public class VendaDAO
     {
-        public int id { get; set; }
-        public DateOnly dataven { get; set; }
-        public float valortotalven { get; set; } 
-        public string statusven { get; set; } = string.Empty;
-        public int idusufk { get; set; }
+        private readonly Conexao _conexao;
 
+        public VendaDAO(Conexao conexao)
+        {
+            _conexao = conexao;
+        }
+        public List<VendaDAO> Listar()
+        {
+            try
+            {
+                var lista = new List<Venda>();
 
-        //id_ven int primary key auto_increment,
-        //data_ven date,
-        //valor_total_ven float,
-        //status_ven varchar(100),
-        //id_usu_fk int
+                using var con = _conexao.GetConnection();
+
+                string sql = "SELECT * FROM categoria";
+                using var comando = con.CreateCommand();
+                comando.CommandText = sql;
+
+                using var leitor = comando.ExecuteReader();
+                while (leitor.Read())
+                {
+                    var venda = new Venda();
+
+                    venda.Id = leitor.GetInt32("id_ven");
+                    venda.Dataven = leitor.GetDateTime("data_ven");
+                    venda.Valortotalven = leitor.GetFloat("valor_total_ven");
+                    venda.Statusven = leitor.GetString("status_ven");
+                    venda.Idusufk = leitor.GetInt16("id_usu_fk");
+
+                    //Aqui estao os de categoria, no seu use o seu                                                                                                                                  
+                    //id_cat int primary key auto_increment,
+                    //nome_cat varchar(300),
+                    //descricao_cat varchar(500),
+                    //codigo_cat varchar(100),
+                    //status_cat varchar(100),
+                    //data_cadastro_cat date
+
+                    lista.Add(venda);
+                }
+                return lista;
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
     }
 }
