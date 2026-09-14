54642fe92633391c933fb4efe91f1b6f6e4b19f2
vinicooo <vinicius8k10@gmail.com>
2026-09-14 10:49:33 -0400

    Update VendaDAO.cs

--- DeltaStock/DAO/VendaDAO.cs
+++ DeltaStock/DAO/VendaDAO.cs
@@ -6,7 +6,7 @@
         public DateOnly dataven { get; set; }
         public float valortotalven { get; set; } 
         public string statusven { get; set; } = string.Empty;
-        public int ideusufk { get; set; }
+        public int idusufk { get; set; }
 
 
         //id_ven int primary key auto_increment,
