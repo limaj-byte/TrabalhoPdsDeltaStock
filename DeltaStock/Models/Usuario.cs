d278bc491bf3fe2a8bd1ac83d4a844772cef886c
Manel <emanuelbatista642s@gmail.com>
2026-09-14 11:05:16 -0400

    Update Usuario.cs

--- DeltaStock/Models/Usuario.cs
+++ DeltaStock/Models/Usuario.cs
@@ -3,20 +3,14 @@
     public class Usuario
     {
 
-        //    CREATE TABLE Produto(
-        //    id_prod int primary key auto_increment,
-        //    codigo_prod varchar(100),
-        //    nome_prod varchar(300),
-        //    descricao_prod varchar(500),
-        //    quantidade_prod int,
-        //    custo_prod float,
-        //    valor_venda_prod float,
-        //    status_prod varchar(100),
-        //    data_cadastro_prod date,
-        //    id_cat_fk int,
-        //    foreign key(id_cat_fk) references Categoria(id_cat),
-        //    id_forn_fk int,
-        //    foreign key(id_forn_fk) references Fornecedor(id_forn)
-        //);
+
+        //id_usu int primary key auto_increment,
+        // nome_usu varchar(300),
+        //email_usu varchar(300),
+        //senha_usu varchar(300),
+        //telefone_usu varchar(100),
+        // endereco_usu varchar(300),
+        //tipo_usu varchar(100),
+        //status_usu varchar(100)
     }
 }
