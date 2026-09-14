79a530686e640cef84cb798b543bfbf76cbc0480
Manel <emanuelbatista642s@gmail.com>
2026-09-14 11:03:00 -0400

    qwerw
    qweqwerq

--- DeltaStock/dao/UsuarioDAO.cs
+++ DeltaStock/dao/UsuarioDAO.cs
@@ -1,8 +1,17 @@
+using static Mysqlx.Expect.Open.Types.Condition.Types;
+
 namespace DeltaStock.DAO
 {
     public class Usuario
     {
 
-
+        //id_usu int primary key auto_increment,
+  // nome_usu varchar(300),
+    //email_usu varchar(300),
+    //senha_usu varchar(300),
+    //telefone_usu varchar(100),
+   // endereco_usu varchar(300),
+    //tipo_usu varchar(100),
+    //status_usu varchar(100)
     }
 }
