49b1a4c99cef6535f5f7d4b61f375dba9be6b28c
GGO <joaogguariento@gmail.com>
2026-09-14 10:59:02 -0400

    Mais limpo

--- DeltaStock/DAO/MovimentacaoDAO.cs
+++ DeltaStock/DAO/MovimentacaoDAO.cs
@@ -47,20 +47,6 @@ namespace DeltaStock.DAO
                         ? null
                         : leitor.GetString("motivo_mov");
 
-                    //id_mov int primary key auto_increment,
-                    //data_mov datetime,
-                    //tipo_mov varchar(100),
-                    //quantidade_mov int,
-                    //saldo_anterior_mov int,
-                    //saldo_final_mov int,
-                    //origem_mov varchar(300),
-                    //id_documento_mov varchar(300),
-                    //motivo_mov varchar(500),
-                    //id_prod_fk int,
-                    //foreign key(id_prod_fk) references Produto(id_prod),
-                    //id_usu_fk int,
-                    //foreign key(id_usu_fk) references Usuario(id_usu)
-
                     lista.Add(movimentacao);
                 }
                 return lista;
